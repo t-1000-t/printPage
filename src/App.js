37a00cdb07c1887fb8a4edc7f7f75e53fbee21bf
t-1000-t <goognya@gmail.com>
2020-01-20 13:56:25 +0200

    print page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Profile from './components/Profile/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
